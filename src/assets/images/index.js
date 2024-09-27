1332c652843fd831ec415f63a77ca8b9994ab8fa
ArfatDivekar2109 <fonetour99@gmail.com>
2024-09-27 19:35:47 +0530

    forth commit

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,89 +1,84 @@
-import bigShoe1 from './big-shoe1.png'
-import bigShoe2 from './big-shoe2.png'
-import bigShoe3 from './big-shoe3.png'
-import iPhone1 from './iPhone1.png'
-import iPhone2 from './iPhone2.webp'
-import iPhone3 from './iPhone3.png'
-import iPhone4 from './iPhone5.png'
-import iPhone10 from './iPhone10.png'
-import iPhone11 from './iPhone11.png'
+import bigShoe1 from "./big-shoe1.png";
+import bigShoe2 from "./big-shoe2.png";
+import bigShoe3 from "./big-shoe3.png";
+import iPhone1 from "./iPhone1.png";
+import iPhone2 from "./iPhone2.webp";
+import iPhone3 from "./iPhone3.png";
+import iPhone4 from "./iPhone5.png";
+import iPhone10 from "./iphone10.png";
+import iPhone11 from "./iPhone11.png";
 
-import customer1 from './customer1.jpeg'
-import customer2 from './customer2.svg'
-import customer3 from './customer3.webp'
+import customer1 from "./customer1.jpeg";
+import customer2 from "./customer2.svg";
+import customer3 from "./customer3.webp";
 
-import footerLogo from './footer.png'
-import headerLogo from './apple.png'
+import footerLogo from "./footer.png";
+import headerLogo from "./apple.png";
 
-import offer from './offer.svg'
+import offer from "./offer.svg";
 
-import shoe4 from './shoe4.svg'
-import shoe5 from './shoe5.svg'
-import shoe6 from './shoe6.svg'
-import shoe7 from './shoe7.svg'
-import shoe8 from './shoe8.svg'
-import macbook from './macbook.png'
-import imac from './imac.png'
-import ipad from './ipad.png'
-import watch from './watch.png'
-import airpods from './airpods.png'
-import iphonebg from './iphone9.png'
-import macpro from './macpro.png'
-import ipad2 from './ipad2.png'
-import macbookpro from './macbook-pro.png'
-import studiodisplay from './studiodisplay.png'
+import shoe4 from "./shoe4.svg";
+import shoe5 from "./shoe5.svg";
+import shoe6 from "./shoe6.svg";
+import shoe7 from "./shoe7.svg";
+import shoe8 from "./shoe8.svg";
+import macbook from "./macbook.png";
+import imac from "./imac.png";
+import ipad from "./ipad.png";
+import watch from "./watch.png";
+import airpods from "./airpods.png";
+import iphonebg from "./iphone9.png";
+import macpro from "./macpro.png";
+import ipad2 from "./ipad2.png";
+import macbookpro from "./macbook-pro.png";
+import studiodisplay from "./studiodisplay.png";
 
-
-import thumbnailBackground from './thumbnail-background.svg'
-import thumbnailShoe1 from './thumbnail-shoe1.svg'
-import thumbnailShoe2 from './thumbnail-shoe2.svg'
-import thumbnailShoe3 from './thumbnail-shoe3.svg'
-import thumbnailphone1 from './iPhone1.png'
-import thumbnailphone2 from './iPhone2.webp'
-import thumbnailphone3 from './iPhone3.png'
-import thumbnailphone4 from './iPhone5.png'
+import thumbnailBackground from "./thumbnail-background.svg";
+import thumbnailShoe1 from "./thumbnail-shoe1.svg";
+import thumbnailShoe2 from "./thumbnail-shoe2.svg";
+import thumbnailShoe3 from "./thumbnail-shoe3.svg";
+import thumbnailphone1 from "./iPhone1.png";
+import thumbnailphone2 from "./iPhone2.webp";
+import thumbnailphone3 from "./iPhone3.png";
+import thumbnailphone4 from "./iPhone5.png";
 
 export {
-    bigShoe1,
-    bigShoe2,
-    bigShoe3,
-    iPhone1,
-    iPhone2,
-    iPhone3,
-    iPhone4,
-    iPhone10,
-    iPhone11,
-
-    customer1,
-    customer2,
-    customer3,
-
-    footerLogo,
-    headerLogo,
-
-    offer,
-    shoe4,
-    shoe5,
-    shoe6,
-    shoe7,
-    shoe8,
-    macbook,
-    imac,
-    ipad,
-    watch,
-    airpods,
-    iphonebg,
-    macpro,
-    ipad2,
-    macbookpro,
-    studiodisplay,
-
-    thumbnailBackground,
-    thumbnailShoe1,
-    thumbnailShoe2,
-    thumbnailShoe3,
-    thumbnailphone1,
-    thumbnailphone2,
-    thumbnailphone3,
-    thumbnailphone4,
-}
+  bigShoe1,
+  bigShoe2,
+  bigShoe3,
+  iPhone1,
+  iPhone2,
+  iPhone3,
+  iPhone4,
+  iPhone10,
+  iPhone11,
+  customer1,
+  customer2,
+  customer3,
+  footerLogo,
+  headerLogo,
+  offer,
+  shoe4,
+  shoe5,
+  shoe6,
+  shoe7,
+  shoe8,
+  macbook,
+  imac,
+  ipad,
+  watch,
+  airpods,
+  iphonebg,
+  macpro,
+  ipad2,
+  macbookpro,
+  studiodisplay,
+  thumbnailBackground,
+  thumbnailShoe1,
+  thumbnailShoe2,
+  thumbnailShoe3,
+  thumbnailphone1,
+  thumbnailphone2,
+  thumbnailphone3,
+  thumbnailphone4,
+};
